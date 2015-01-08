1cb2b77c221e67279625342476c7132ddbcd9ecb
fishermanswharff <fishermanswharff@github.com>
2015-01-07 22:02:04 -0500

    spec file

--- spec/spec_spec.rb
+++ spec/spec_spec.rb
@@ -4,3 +4,10 @@ RSpec.describe "my first method" do
     expect(my_first_method(212)).to eq 100
   end
 end
+
+RSpec.describe "my second method" do
+  it 'does something really cool' do
+    expect(my_second_method(32)).to eq 0
+    expect(my_second_method(212)).to eq 100
+  end
+end
